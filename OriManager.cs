01c47d364998408bf8721df52090b320e80f0ec0
Timo Schwarzer <me@timoschwarzer.com>
2016-09-18 16:00:37 +0200

    Add OriMap

--- OriManager.cs
+++ OriManager.cs
@@ -5,11 +5,15 @@ using System.Drawing;
 using System.IO;
 using System.Threading;
 using System.Windows.Forms;
+using OriMap;
+
 namespace LiveSplit.OriDE {
 	public partial class OriManager : Form {
 		public OriMemory Memory { get; set; }
 		private bool useLivesplitColors = true, extraFast = false, goingFast = false;
 		private KeyboardHook kbHook;
+        private OriMapWindow mapWindow;
+
 		public OriManager() {
 			this.DoubleBuffered = true;
 			kbHook = new KeyboardHook(KeyboardHook.Parameters.PassAllKeysToNextApp);
@@ -66,14 +70,23 @@ namespace LiveSplit.OriDE {
 					lblNextInput.Visible = true;
 					lblTASStates.Visible = true;
 				}
-			}
+			} else if (e.Control && e.KeyCode == Keys.M) {
+                if (this.mapWindow == null) {
+                    this.mapWindow = new OriMapWindow();
+                    this.mapWindow.Show();
+                } else if (this.mapWindow.IsVisible) {
+                    this.mapWindow.Hide();
+                } else {
+                    this.mapWindow.Show();
+                }
+            }
 		}
 
 		private void UpdateLoop() {
 			bool lastHooked = false;
-			while (true) {
+            bool hooked = Memory.HookProcess();
+            while (true) {
 				try {
-					bool hooked = Memory.HookProcess();
 					if (hooked) {
 						UpdateValues();
 					}
@@ -89,84 +102,90 @@ namespace LiveSplit.OriDE {
 			if (this.InvokeRequired) {
 				this.Invoke((Action)UpdateValues);
 			} else {
-				bool tasEnabled = this.Width == 650 || (Memory.GetTASState() & 1) != 0;
-				if (this.Width < 650 && tasEnabled) {
-					this.Width = 650;
-					this.Height = 235;
-					lblCurrentInput.Visible = true;
-					lblNextInput.Visible = true;
-					lblTASStates.Visible = true;
-				} else {
-					lblCurrentInput.Text = Memory.GetTASCurrentInput();
-					lblNextInput.Text = Memory.GetTASNextInput();
-					lblTASStates.Text = Memory.GetTASExtraInfo();
-				}
-				GameState gameState = Memory.GetGameState();
-				bool isInGameWorld = CheckInGameWorld(gameState);
-				bool isStartingGame = CheckStartingNewGame(gameState);
-				PointF currentSpeed = Memory.CurrentSpeed();
-				PointF pos = Memory.HasTAS() ? Memory.GetTASOriPositon() : Memory.GetCameraTargetPosition();
-				HitBox ori = new HitBox(pos, 0.68f, 1.15f, true);
-				HitBox hitBox = new HitBox("145,580,20,40");
-				HitBox hitBox2 = new HitBox("170,580,130,140");
-				bool inFinal = hitBox.Intersects(ori) || hitBox2.Intersects(ori);
+                bool tasEnabled = this.Width == 650 || (Memory.GetTASState() & 1) != 0;
+                if (this.Width < 650 && tasEnabled) {
+                    this.Width = 650;
+                    this.Height = 235;
+                    lblCurrentInput.Visible = true;
+                    lblNextInput.Visible = true;
+                    lblTASStates.Visible = true;
+                } else {
+                    lblCurrentInput.Text = Memory.GetTASCurrentInput();
+                    lblNextInput.Text = Memory.GetTASNextInput();
+                    lblTASStates.Text = Memory.GetTASExtraInfo();
+                }
+                GameState gameState = Memory.GetGameState();
+                bool isInGameWorld = CheckInGameWorld(gameState);
+                bool isStartingGame = CheckStartingNewGame(gameState);
+                PointF currentSpeed = Memory.CurrentSpeed();
 
-				if (extraFast && Math.Abs(Memory.WaterSpeed() - 9f) > 0.1f && !inFinal) {
-					goingFast = true;
-					Memory.SetSpeed(24f, 85f, 39f, 12f, 70f, 70f, 9f, 60f, 200f, 5f, 16f, 90f, 180f);
-				} else if ((!extraFast || inFinal) && goingFast) {
-					goingFast = false;
-					Memory.SetSpeed(11.6667f, 60f, 26f, 6f, 56.568f, 40f, 6f, 38f, 100f, 3f, 8f, 50f, 100f);
-				}
+                PointF pos = Memory.HasTAS() ? Memory.GetTASOriPositon() : Memory.GetCameraTargetPosition();
 
-				List<Area> areas = Memory.GetMapCompletion();
-				decimal total = 0;
-				Area currentArea = default(Area);
-				for (int i = 0; i < areas.Count; i++) {
-					Area area = areas[i];
-					total += area.Progress;
-					if (area.Current) {
-						currentArea = area;
-					}
-				}
-				if (areas.Count > 0) {
-					total /= areas.Count;
-				}
+                if (this.mapWindow != null) {
+                    this.mapWindow.setPos(pos);
+                }
 
-				List<Scene> scenes = Memory.GetScenes();
-				string currentScene = string.Empty;
-				for (int i = 0; i < scenes.Count; i++) {
-					Scene scene = scenes[i];
-					if (scene.State == SceneState.Loaded) {
-						currentScene = scene.Name;
-						break;
-					}
-				}
-				lblArea.Text = "Area: " + (string.IsNullOrEmpty(currentArea.Name) ? "N/A" : currentArea.Name + " - " + currentArea.Progress.ToString("0.00") + "%");
-				lblMap.Text = "Total: " + total.ToString("0.00") + "% Scene: " + currentScene;
-				lblPos.Text = "Pos: " + pos.X.ToString("0.000") + ", " + pos.Y.ToString("0.000");
-				lblSpeed.Text = (extraFast ? "Insane Speed: " : "Speed: ") + currentSpeed.X.ToString("0.000") + ", " + currentSpeed.Y.ToString("0.000") + " (" + Math.Sqrt(currentSpeed.X * currentSpeed.X + currentSpeed.Y * currentSpeed.Y).ToString("0.000") + ")";
-				PointF cursor = Memory.GetCursorPosition();
-				lblCursorPosition.Text = "Cursor: " + cursor.X.ToString("0.000") + ", " + cursor.Y.ToString("0.000");
+                HitBox ori = new HitBox(pos, 0.68f, 1.15f, true);
+                HitBox hitBox = new HitBox("145,580,20,40");
+                HitBox hitBox2 = new HitBox("170,580,130,140");
+                bool inFinal = hitBox.Intersects(ori) || hitBox2.Intersects(ori);
 
-				if (isInGameWorld) {
-					int level = Memory.GetCurrentLevel();
-					int xp = Memory.GetExperience();
-					lblLevel.Text = "Level: " + level.ToString();
-					lblHP.Text = "HP: " + ((double)Memory.GetCurrentHP() / 4).ToString("0.##") + " / " + Memory.GetCurrentHPMax().ToString();
-					lblEN.Text = "EN: " + Memory.GetCurrentEN().ToString("0.##") + " / " + ((int)Memory.GetCurrentENMax()).ToString();
-					lblAbility.Text = "Ability: " + Memory.GetAbilityCells().ToString() + " / 33";
-					lblXP.Text = "XP: " + xp.ToString() + " / " + GetXP(level);
-					lblKeys.Text = "Keys: " + Memory.GetKeyStones();
-				} else {
-					lblLevel.Text = "Level: N/A";
-					lblHP.Text = "HP: N/A";
-					lblEN.Text = "EN: N/A";
-					lblAbility.Text = "Ability: N/A";
-					lblXP.Text = "XP: N/A";
-					lblKeys.Text = "Keys: N/A";
-				}
-			}
+                if (extraFast && Math.Abs(Memory.WaterSpeed() - 9f) > 0.1f && !inFinal) {
+                    goingFast = true;
+                    Memory.SetSpeed(24f, 85f, 39f, 12f, 70f, 70f, 9f, 60f, 200f, 5f, 16f, 90f, 180f);
+                } else if ((!extraFast || inFinal) && goingFast) {
+                    goingFast = false;
+                    Memory.SetSpeed(11.6667f, 60f, 26f, 6f, 56.568f, 40f, 6f, 38f, 100f, 3f, 8f, 50f, 100f);
+                }
+
+                List<Area> areas = Memory.GetMapCompletion();
+                decimal total = 0;
+                Area currentArea = default(Area);
+                for (int i = 0; i < areas.Count; i++) {
+                    Area area = areas[i];
+                    total += area.Progress;
+                    if (area.Current) {
+                        currentArea = area;
+                    }
+                }
+                if (areas.Count > 0) {
+                    total /= areas.Count;
+                }
+
+                List<Scene> scenes = Memory.GetScenes();
+                string currentScene = string.Empty;
+                for (int i = 0; i < scenes.Count; i++) {
+                    Scene scene = scenes[i];
+                    if (scene.State == SceneState.Loaded) {
+                        currentScene = scene.Name;
+                        break;
+                    }
+                }
+                lblArea.Text = "Area: " + (string.IsNullOrEmpty(currentArea.Name) ? "N/A" : currentArea.Name + " - " + currentArea.Progress.ToString("0.00") + "%");
+                lblMap.Text = "Total: " + total.ToString("0.00") + "% Scene: " + currentScene;
+                lblPos.Text = "Pos: " + pos.X.ToString("0.000") + ", " + pos.Y.ToString("0.000");
+                lblSpeed.Text = (extraFast ? "Insane Speed: " : "Speed: ") + currentSpeed.X.ToString("0.000") + ", " + currentSpeed.Y.ToString("0.000") + " (" + Math.Sqrt(currentSpeed.X * currentSpeed.X + currentSpeed.Y * currentSpeed.Y).ToString("0.000") + ")";
+                PointF cursor = Memory.GetCursorPosition();
+                lblCursorPosition.Text = "Cursor: " + cursor.X.ToString("0.000") + ", " + cursor.Y.ToString("0.000");
+
+                if (isInGameWorld) {
+                    int level = Memory.GetCurrentLevel();
+                    int xp = Memory.GetExperience();
+                    lblLevel.Text = "Level: " + level.ToString();
+                    lblHP.Text = "HP: " + ((double)Memory.GetCurrentHP() / 4).ToString("0.##") + " / " + Memory.GetCurrentHPMax().ToString();
+                    lblEN.Text = "EN: " + Memory.GetCurrentEN().ToString("0.##") + " / " + ((int)Memory.GetCurrentENMax()).ToString();
+                    lblAbility.Text = "Ability: " + Memory.GetAbilityCells().ToString() + " / 33";
+                    lblXP.Text = "XP: " + xp.ToString() + " / " + GetXP(level);
+                    lblKeys.Text = "Keys: " + Memory.GetKeyStones();
+                } else {
+                    lblLevel.Text = "Level: N/A";
+                    lblHP.Text = "HP: N/A";
+                    lblEN.Text = "EN: N/A";
+                    lblAbility.Text = "Ability: N/A";
+                    lblXP.Text = "XP: N/A";
+                    lblKeys.Text = "Keys: N/A";
+                }
+            }
 		}
 		public bool CheckInGame(GameState state) {
 			return state != GameState.Logos && state != GameState.StartScreen && state != GameState.TitleScreen;
---
+++ OriMap/OriMapWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OriMap {
+    /// <summary>
+    /// Interaktionslogik für OriMap.xaml
+    /// </summary>
+    public partial class OriMapWindow : Window {
+        private TranslateTransform mapTransform;
+        const double FACTOR_X = 4.11;
+        const double FACTOR_Y = 4.12;
+        const float TRANSFORM_TO_ORIGIN_X = -4509;
+        const float TRANSFORM_TO_ORIGIN_Y = -3975;
+
+        public OriMapWindow() {
+            InitializeComponent();
+            mapTransform = new TranslateTransform();
+            mapImage.RenderTransform = mapTransform;
+        }
+
+        public void setPos(System.Drawing.PointF pos) {
+            mapTransform.X = ((-pos.X) * FACTOR_X + TRANSFORM_TO_ORIGIN_X) + this.ActualWidth / 2;
+            mapTransform.Y = ((pos.Y) * FACTOR_Y + TRANSFORM_TO_ORIGIN_Y) + this.ActualHeight / 2;
+        }
+    }
+}
